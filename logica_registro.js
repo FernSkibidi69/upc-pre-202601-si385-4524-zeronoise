7e3fdb5d97d8c74eda1d26deed945d3d61c8c72f
Mauricio1608 <u202419277@upc.edu.pe>
2026-07-09 17:43:12 -0500

    Update logica_registro.js

--- logica_registro.js
+++ logica_registro.js
@@ -3,7 +3,6 @@ const vistaLogin = document.getElementById('vista-login');
 const linkIrLogin = document.getElementById('link-ir-login');
 const linkIrRegistro = document.getElementById('link-ir-registro');
 
-// Intercambio de vistas (Registro <-> Login)
 if (linkIrLogin && linkIrRegistro) {
   linkIrLogin.addEventListener('click', (e) => {
     e.preventDefault();
@@ -18,22 +17,18 @@ if (linkIrLogin && linkIrRegistro) {
   });
 }
 
-// Acción al enviar el formulario de Registro
 document.getElementById('form-registro').addEventListener('submit', (e) => {
   e.preventDefault();
   alert('¡Cuenta ciudadana creada con éxito! Procede a iniciar sesión.');
   
-  // Tras registrar, lo lleva automáticamente a la pantalla de Login
   vistaRegistro.style.display = 'none';
   vistaLogin.style.display = 'block';
 });
 
-// Acción al enviar el formulario de Login
 document.getElementById('form-login').addEventListener('submit', (e) => {
   e.preventDefault();
   alert('¡Inicio de sesión correcto! Redirigiendo a la plataforma NoiseGuard...');
   
-  // Guarda un estado en la sesión del navegador para simular la persistencia
   localStorage.setItem('ciudadanoLogueado', 'true');
   
   // Redirecciona al usuario a la landing page principal automáticamente
