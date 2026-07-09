4743b0bd189e35e6955129ed5ba6bcd5afc98e2b
Mauricio1608 <u202419277@upc.edu.pe>
2026-07-09 18:36:59 -0500

    Update logica_registro.js

--- logica_registro.js
+++ logica_registro.js
@@ -1,44 +1,21 @@
-const vistaLogin = document.getElementById('vista-login');
-const vistaRegistro = document.getElementById('vista-registro');
+document.addEventListener("DOMContentLoaded", () => {
+  const vistaLogin = document.getElementById("vista-login");
+  const vistaRegistro = document.getElementById("vista-registro");
+  
+  const irARegistro = document.getElementById("link-ir-registro");
+  const irALogin = document.getElementById("link-ir-login");
 
-const navIrLogin = document.getElementById('nav-ir-login');
-const navIrRegistro = document.getElementById('nav-ir-registro');
+  // Cambiar a vista de Registro
+  irARegistro.addEventListener("click", (e) => {
+    e.preventDefault();
+    vistaLogin.style.display = "none";
+    vistaRegistro.style.display = "block";
+  });
 
-const linkSwitchReg = document.getElementById('link-switch-reg');
-const linkSwitchLog = document.getElementById('link-switch-log');
-
-function mostrarLogin() {
-  vistaRegistro.style.display = 'none';
-  vistaLogin.style.display = 'block';
-  navIrLogin.classList.add('active');
-  navIrRegistro.classList.remove('active');
-}
-
-function mostrarRegistro() {
-  vistaLogin.style.display = 'none';
-  vistaRegistro.style.display = 'block';
-  navIrRegistro.classList.add('active');
-  navIrLogin.classList.remove('active');
-}
-
-// Eventos de los botones del panel izquierdo
-navIrLogin.addEventListener('click', mostrarLogin);
-navIrRegistro.addEventListener('click', mostrarRegistro);
-
-// Eventos de los enlaces rápidos inferiores
-linkSwitchReg.addEventListener('click', (e) => { e.preventDefault(); mostrarRegistro(); });
-linkSwitchLog.addEventListener('click', (e) => { e.preventDefault(); mostrarLogin(); });
-
-// Simulación funcional formularios
-document.getElementById('form-registro').addEventListener('submit', (e) => {
-  e.preventDefault();
-  alert('¡Cuenta ciudadana creada con éxito! Procede a iniciar sesión.');
-  mostrarLogin();
-});
-
-document.getElementById('form-login').addEventListener('submit', (e) => {
-  e.preventDefault();
-  alert('¡Inicio de sesión correcto! Redirigiendo a NoiseGuard...');
-  localStorage.setItem('ciudadanoLogueado', 'true');
-  window.location.href = 'index.html';
+  // Cambiar a vista de Login
+  irALogin.addEventListener("click", (e) => {
+    e.preventDefault();
+    vistaRegistro.style.display = "none";
+    vistaLogin.style.display = "block";
+  });
 });
